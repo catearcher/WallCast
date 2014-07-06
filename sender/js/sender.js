5240e1681375d8f4b5bb5a0fed9ae51b0a3275bb
catearcher <indieboy86@gmail.com>
2014-07-06 21:15:55 +0200

    sender can now start the receiver

---
+++ sender/js/sender.js
@@ -0,0 +1,61 @@
+/*jshint jquery:true, undef:true, devel:true, indent:2, curly:true, strict:true, browser:true */
+/*global chrome */
+
+(function() {
+  "use strict";
+
+  var
+  session,
+  appId = "41F577EA",
+  namespace = "urn:x-cast:at.diesocialisten.dev.wallcast",
+  initInterval,
+
+  initializeCastApi = function() {
+    var
+    sessionRequest = new chrome.cast.SessionRequest(appId),
+    apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
+
+    chrome.cast.initialize(apiConfig, function() {
+      $(".connect-chromecast").removeClass("hidden");
+    }, onError);
+  },
+
+  sessionListener = function(e) {
+    session = e;
+    session.addUpdateListener(sessionUpdateListener);
+    session.addMessageListener(namespace, receiverMessage);
+  },
+
+  receiverListener = function(e) {
+
+  },
+
+  sessionUpdateListener = function(isAlive) {
+    if (!isAlive) {
+      session = null;
+    }
+  },
+
+  receiverMessage = function(namespace, message) {
+    console.log(message);
+  },
+
+  onError = function(message) {
+    console.error(message);
+  };
+
+  initInterval = setInterval(function() {
+    if (chrome.cast && chrome.cast.isAvailable) {
+      initializeCastApi();
+      clearInterval(initInterval);
+    }
+  }, 500);
+
+  $(".connect-chromecast").on("click", function() {
+    chrome.cast.requestSession(function(e) {
+      session = e;
+    }, onError);
+
+    return false;
+  });
+}());
